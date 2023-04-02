7777bb4fcc3c1843c53fd54a77327e3778a2de1c
Sukumar Ganesan <suku1612@gmail.com>
2023-04-03 01:39:26 +0530

    minor refactoring

--- app/api/power_user.py
+++ app/api/power_user.py
@@ -1,11 +1,19 @@
 import tempfile
 from typing import Annotated
 
-from fastapi import APIRouter, Depends, status, Response
-from starlette.responses import StreamingResponse
+from fastapi import APIRouter, Depends, status
+from starlette.responses import StreamingResponse, JSONResponse
 
-from app.db.models import ReportModel, ReportUpdateModel
+from app.db.crud import ReportCRUD
+from app.db.models import (
+    ReportModel,
+    TokenData,
+    ReportStatus,
+    UserModel,
+    COEModel,
+)
 from app.dependencies import DependencyContainer
+from app.services.auth import parse_token
 from app.services.user_services import UserService, PowerUserService
 
 router = APIRouter()
@@ -14,7 +22,7 @@ router = APIRouter()
 @router.get("/power-user/all-users", tags=["power_user"])
 async def get_all_user(
     dependency_container: Annotated[DependencyContainer, Depends(DependencyContainer)],
-) -> list[ReportModel]:
+) -> list[UserModel]:
     with dependency_container.get_db() as db:
         reports = PowerUserService.fetch_all_users(db)
 
@@ -24,7 +32,7 @@ async def get_all_user(
 @router.get("/power-user/all-coe", tags=["power_user"])
 async def get_all_coe(
     dependency_container: Annotated[DependencyContainer, Depends(DependencyContainer)],
-) -> list[ReportModel]:
+) -> list[COEModel]:
     with dependency_container.get_db() as db:
         reports = PowerUserService.fetch_all_coe(db)
 
@@ -45,17 +53,27 @@ async def get_all_reports(
 async def generate_report(
     report_id: int,
     dependency_container: Annotated[DependencyContainer, Depends(DependencyContainer)],
+    token_data: TokenData = Depends(parse_token),
 ):
     with dependency_container.get_db() as db:
         with tempfile.NamedTemporaryFile(suffix=".pdf") as temp:
             report_generator = dependency_container.REPORT_GENERATOR
+
+            report = ReportCRUD.get_report_by_report_id(db, report_id)
+            if report.report_status == ReportStatus.Draft:
+                return JSONResponse(
+                    content={"message": "report is a draft"},
+                    status_code=status.HTTP_404_NOT_FOUND,
+                )
+
             file_contents = UserService.generate_pdf(
-                db, report_id, report_generator=report_generator, file_name=temp.name
+                db, report, report_generator=report_generator, file_name=temp.name
             )
             headers = {
                 "Content-Disposition": "attachment; filename=report.pdf",
                 "Content-Type": "application/pdf",
             }
+            print(file_contents)
             response = StreamingResponse(
                 content=file_contents, status_code=status.HTTP_200_OK, headers=headers
             )
