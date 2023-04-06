23b0e5044068302d728a5ffb0383ba43f77ef85f
Sukumar Ganesan <suku1612@gmail.com>
2023-04-07 00:47:47 +0530

    removed generate report api from poweruser and admin

--- app/api/power_user.py
+++ app/api/power_user.py
@@ -57,37 +57,3 @@ async def get_all_reports(
         reports = PowerUserService.fetch_all_reports(db)
 
     return reports
-
-
-@router.get("/power-user/generate-report", tags=["power_user"])
-async def generate_report(
-    report_id: int,
-    dependency_container: Annotated[DependencyContainer, Depends(DependencyContainer)],
-    token_data: TokenData = Depends(parse_token),
-):
-    if token_data.permissions != UserLevels.PowerUser:
-        return Response(status_code=status.HTTP_401_UNAUTHORIZED)
-
-    with dependency_container.get_db() as db:
-        with tempfile.NamedTemporaryFile(suffix=".pdf") as temp:
-            report_generator = dependency_container.REPORT_GENERATOR
-
-            report = ReportCRUD.get_report_by_report_id(db, report_id)
-            if report.report_status == ReportStatus.Draft:
-                return JSONResponse(
-                    content={"message": "report is a draft"},
-                    status_code=status.HTTP_404_NOT_FOUND,
-                )
-
-            file_contents = UserService.generate_pdf(
-                db, report, report_generator=report_generator, file_name=temp.name
-            )
-            headers = {
-                "Content-Disposition": "attachment; filename=report.pdf",
-                "Content-Type": "application/pdf",
-            }
-            print(file_contents)
-            response = StreamingResponse(
-                content=file_contents, status_code=status.HTTP_200_OK, headers=headers
-            )
-            return response
